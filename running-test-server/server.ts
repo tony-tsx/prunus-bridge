ff1cac9a923bbf545d50baa783893d7d56e773a5
Tony <tony.js@zoho.eu>
2021-01-13 18:17:13 -0300

    fix: wrong handler

--- running-test-server/server.ts
+++ running-test-server/server.ts
@@ -1,3 +1,4 @@
+import bodyParser from 'body-parser'
 import cors from 'cors'
 import Express from 'express'
 import { createConnection } from 'typeorm'
@@ -6,6 +7,8 @@ import TypeORMBridge from '../src'
 import User from './bridges/User'
 
 const app = Express()
+
+app.use( bodyParser.json() )
 app.use( cors() )
 app.use( TypeORMBridge.router( User ) )
 
