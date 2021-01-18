c8c9a4f00fb6ea7c9be6e3959979dcde3e9c2959
Tony <tony.js@zoho.eu>
2021-01-18 12:16:29 -0300

    feat: check configuration file in src and dist folders

--- src/helpers/get-connection.ts
+++ src/helpers/get-connection.ts
@@ -2,11 +2,24 @@ import { PathLike } from 'fs'
 
 import { _require } from './_import'
 import getTypeORM from './get-typeorm'
+import isClientSide from './is-client-side'
 
 type fs = typeof import( 'fs' )
 type fsPromise = typeof import( 'fs/promises' )
 type path = typeof import( 'path' )
 
+const files = [
+  !isClientSide() && process.env.TYPEORM_CONFIG,
+  'ormconfig.json',
+  'ormconfig.ts',
+  'ormconfig.js' 
+].filter( Boolean )
+
+if ( !isClientSide() )
+  files.push(
+    ...files.map( file => _require<path>( 'path' ).join( 'dist', file ) ),
+    ...files.map( file => _require<path>( 'path' ).join( 'src', file ) )
+  )
 
 const checkIfExists = ( path: PathLike ) => {
   const fs = _require<fsPromise>( 'fs/promises' )
@@ -23,7 +36,6 @@ const getConnection = async ( connectionName?: string ) => {
     } catch ( e ) {
       const { resolve } = _require<path>( 'path' )
 
-      const files = [ 'ormconfig.json', 'ormconfig.ts' ]
       for await ( const file of files ) {
         const fullpath = resolve( file )
         const exists = await checkIfExists( resolve( file ) )
@@ -44,7 +56,6 @@ getConnection.sync = ( connectionName?: string ) => {
       const fs = _require<fs>( 'fs' )
       const { resolve } = _require<path>( 'path' )
 
-      const files = [ 'ormconfig.json', 'ormconfig.ts', 'ormconfig.js' ]
       for ( const file of files ) {
         const fullpath = resolve( file )
         if (
