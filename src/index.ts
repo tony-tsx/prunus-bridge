1b1b99dbda9179543c14398b426c16982551fca5
Tony <tony.js@zoho.eu>
2021-01-26 18:19:19 -0300

    feat: add support to group handler

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,23 @@
 import createBridge from './create-bridge'
+import { Configuration } from './create-handler'
 import { _require } from './helpers/_import'
 import Bridge from './types/bridge'
 import { AnyTarget } from './types/helpers'
 
-type createHandle = typeof import( './handlers' )
+type createRouter = typeof import( './handlers' )
 type createCommand = typeof import( './cli' )
-
+type createHandler = typeof import( './create-handler' )
 namespace TypeORMBridge {
   export const create = createBridge
+
   export const command = <E extends AnyTarget, S, I>( bridge: Bridge<E, S, I> ) =>
     new ( _require<createCommand>( './cli', __dirname ).default )( bridge as any )
+
   export const router = <E extends AnyTarget, S, I>( bridge: Bridge<E, S, I> ) =>
-    _require<createHandle>( './handlers', __dirname ).default( bridge as any )
+    _require<createRouter>( './handlers', __dirname ).default( bridge as any )
+
+  export const handler = ( config?: Configuration ) =>
+    _require<createHandler>( './create-handler', __dirname ).default( config )
 }
 
 export = TypeORMBridge
