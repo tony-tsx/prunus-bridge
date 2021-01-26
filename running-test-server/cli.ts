06972b153dbab8df4974ad54dc7ee9ed4ad7460b
Tony <tony.js@zoho.eu>
2021-01-26 18:17:03 -0300

    rewrite: remove separate test folders

--- running-test-server/cli.ts
+++
@@ -1,8 +0,0 @@
-import commander from 'commander'
-
-import TypeORMBridge from '../src'
-import User from './bridges/User'
-
-commander.addCommand( TypeORMBridge.command( User ).alias( 'user' ) )
-
-commander.parse( process.argv )
