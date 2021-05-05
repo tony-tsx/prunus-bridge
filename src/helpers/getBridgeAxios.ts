58c53ee9ad7e96bfa15271773cad0ccbbee5c1c4
Tony <tony.js@zoho.eu>
2021-05-05 01:55:19 -0300

    fix: force axios typing

--- src/helpers/getBridgeAxios.ts
+++ src/helpers/getBridgeAxios.ts
@@ -1,10 +1,11 @@
+import { AxiosInstance } from 'axios'
 import { AnyBridge, AnyBridgeInstance } from '../typings/bridge'
 import { getBridgeOptions } from './getBridgeOptions'
 import { getAxios } from './internals/getAxios'
 
-const getBridgeAxios = async <T extends AnyBridge | AnyBridgeInstance>( bridge: T ) => {
+const getBridgeAxios = async <T extends AnyBridge | AnyBridgeInstance>( bridge: T ): Promise<AxiosInstance> => {
   const options = getBridgeOptions( bridge )
-  return await options.getAxiosInstance?.() ?? await getAxios()
+  return await options.getAxiosInstance?.() ?? await getAxios().then( module => module.default )
 }
 
 export { getBridgeAxios }
